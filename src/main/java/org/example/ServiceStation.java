52550fe5204d70d19380c1fb7072fbe9e2774ccb
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-04-28 17:17:33 +0500

    Update ServiceStation.java

--- src/main/java/org/example/ServiceStation.java
+++ src/main/java/org/example/ServiceStation.java
@@ -9,11 +9,11 @@ public class ServiceStation
             System.out.println("Обслуживаем " + car.modelName);
             for (int i = 0; i < car.wheelsCount; i++)
             {
-<<<<<<< HEAD
-                car.updateTyre();
-=======
+
+                //car.updateTyre();
+
                 Transport.updateTyre();
->>>>>>> 7168f05 (Initial commit)
+
             }
             car.checkEngine();
         } else if (truck != null)
@@ -21,11 +21,11 @@ public class ServiceStation
             System.out.println("Обслуживаем " + truck.modelName);
             for (int i = 0; i < truck.wheelsCount; i++)
             {
-<<<<<<< HEAD
-                truck.updateTyre();
-=======
+
+                //truck.updateTyre();
+
                 Transport.updateTyre();
->>>>>>> 7168f05 (Initial commit)
+
             }
             truck.checkEngine();
             truck.checkTrailer();
@@ -34,11 +34,11 @@ public class ServiceStation
             System.out.println("Обслуживаем " + bicycle.modelName);
             for (int i = 0; i < bicycle.wheelsCount; i++)
             {
-<<<<<<< HEAD
-                bicycle.updateTyre();
-=======
+
+                //bicycle.updateTyre();
+
                 Transport.updateTyre();
->>>>>>> 7168f05 (Initial commit)
+
             }
         }
     }

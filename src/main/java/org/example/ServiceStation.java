7e1be4ab3ab8ddee6af7937dee94d33a460c465f
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-05-17 18:42:00 +0500

    Update ServiceStation.java

--- src/main/java/org/example/ServiceStation.java
+++ src/main/java/org/example/ServiceStation.java
@@ -1,41 +1,41 @@
-package org.example;
-
-public class ServiceStation extends Transport
+public class ServiceStation
 {
     public void check(Car car)
     {
         if (car != null)
         {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++)
+            System.out.println("Обслуживаем " + car.getModelName());
+            for (int i = 0; i < car.getWheelsCount(); i++)
             {
-                //car.updateTyre();
-                Transport.updateTyre();
+                car.updateTyre();
             }
             car.checkEngine();
         }
     }
-        //else if (truck != null)
-        public void check(Truck truck)
+
+    public void check(Truck truck)
+    {
+        if (truck != null)
         {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++)
+            System.out.println("Обслуживаем " + truck.getModelName());
+            for (int i = 0; i < truck.getWheelsCount(); i++)
             {
-                //truck.updateTyre();
-                Transport.updateTyre();
+                truck.updateTyre();
             }
             truck.checkEngine();
             truck.checkTrailer();
         }
-        //else if (bicycle != null)
-        public void check(Bicycle bicycle)
+    }
+
+    public void check(Bicycle bicycle)
+    {
+        if (bicycle != null)
         {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++)
+            System.out.println("Обслуживаем " + bicycle.getModelName());
+            for (int i = 0; i < bicycle.getWheelsCount(); i++)
             {
-                //bicycle.updateTyre();
-                Transport.updateTyre();
+                bicycle.updateTyre();
             }
         }
+    }
 }
-

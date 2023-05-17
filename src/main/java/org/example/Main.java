5d1fb0fe9a216c390c9762b40ccaa100b1be6a94
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-05-17 18:42:31 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,33 @@
-package org.example;
-
-public class Main
-{
+public class Main {
     public static void main(String[] args)
     {
-        // System.out.println("Hello world!");
+        Car car = new Car("Лада", 4);
+        Car car2 = new Car("Мазератти", 4);
+        car.setModelName("car1");
+        car2.setModelName("car2");
+        car.setWheelsCount(4);
+        car2.setWheelsCount(4);
 
-        Car car = new Car();
-        Car car2 = new Car();
-        car.modelName = "car1";
-        car2.modelName = "car2";
-        car.wheelsCount = 4;
-        car2.wheelsCount = 4;
+        Truck truck = new Truck("Белаз", 6);
+        Truck truck2 = new Truck("Камаз", 6);
+        truck.setModelName("truck1");
+        truck2.setModelName("truck2");
+        truck.setWheelsCount(6);
+        truck2.setWheelsCount(8);
 
-        Truck truck = new Truck();
-        Truck truck2 = new Truck();
-        truck.modelName = "truck1";
-        truck2.modelName = "truck2";
-        truck.wheelsCount = 6;
-        truck2.wheelsCount = 8;
-
-        Bicycle bicycle = new Bicycle();
-        Bicycle bicycle2 = new Bicycle();
-        bicycle.modelName = "bicycle1";
-        bicycle2.modelName = "bicycle2";
-        bicycle.wheelsCount = 2;
-        bicycle2.wheelsCount = 2;
+        Bicycle bicycle = new Bicycle("Минск", 2);
+        Bicycle bicycle2 = new Bicycle("Иж", 3);
+        bicycle.setModelName("bicycle1");
+        bicycle2.setModelName("bicycle2");
+        bicycle.setWheelsCount(2);
+        bicycle2.setWheelsCount(2);
 
         ServiceStation station = new ServiceStation();
-        station.check(car, null, null);
-        station.check(car2, null, null);
-        station.check(null, bicycle, null);
-        station.check(null, bicycle2, null);
-        station.check(null, null, truck);
-        station.check(null, null, truck2);
-
+        station.check(car);
+        station.check(car2);
+        station.check(bicycle);
+        station.check(bicycle2);
+        station.check(truck);
+        station.check(truck2);
     }
 }

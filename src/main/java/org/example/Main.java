383749b72df9f65f521ddf2b3e745aaa526b6042
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-05-19 00:33:39 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,27 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
         Car car = new Car("Лада", 4);
         Car car2 = new Car("Мазератти", 4);
-        car.setModelName("car1");
-        car2.setModelName("car2");
-        car.setWheelsCount(4);
-        car2.setWheelsCount(4);
+      //  car.setModelName("car1");
+      //  car2.setModelName("car2");
+      //  car.setWheelsCount(4);
+      //  car2.setWheelsCount(4);
 
         Truck truck = new Truck("Белаз", 6);
         Truck truck2 = new Truck("Камаз", 6);
-        truck.setModelName("truck1");
-        truck2.setModelName("truck2");
-        truck.setWheelsCount(6);
-        truck2.setWheelsCount(8);
+      //  truck.setModelName("truck1");
+      //  truck2.setModelName("truck2");
+      //  truck.setWheelsCount(6);
+      //  truck2.setWheelsCount(8);
 
         Bicycle bicycle = new Bicycle("Минск", 2);
         Bicycle bicycle2 = new Bicycle("Иж", 3);
-        bicycle.setModelName("bicycle1");
-        bicycle2.setModelName("bicycle2");
-        bicycle.setWheelsCount(2);
-        bicycle2.setWheelsCount(2);
+      //  bicycle.setModelName("bicycle1");
+      //  bicycle2.setModelName("bicycle2");
+      //  bicycle.setWheelsCount(2);
+     //   bicycle2.setWheelsCount(2);
 
         ServiceStation station = new ServiceStation();
         station.check(car);
@@ -29,5 +30,7 @@ public class Main {
         station.check(bicycle2);
         station.check(truck);
         station.check(truck2);
+
     }
+
 }

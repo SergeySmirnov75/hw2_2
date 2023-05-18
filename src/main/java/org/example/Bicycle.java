26683da0f8d2620a55dfdcc2da792de80a80fa12
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-05-19 00:32:45 +0500

    Update Bicycle.java

--- src/main/java/org/example/Bicycle.java
+++ src/main/java/org/example/Bicycle.java
@@ -1,12 +1,21 @@
 public class Bicycle extends Transport
 {
-    public void updateTyre()
-    {
-        System.out.println("Меняем покрышку велосипеду");
-    }
+
     public Bicycle(String modelName, int wheelsCount)
     {
         super(modelName, wheelsCount);
+    }
+
+    public void check()
+    {
+        if (this != null)
+        {
+            //System.out.println("Обслуживаем " + this.getModelName());
+            for (int i = 0; i < this.getWheelsCount(); i++)
+            {
+                this.updateTyre();
+            }
+        }
     }
 
 }

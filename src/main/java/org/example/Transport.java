4c57bb61fb59d797f8a85465ce8430c5d89cc7ab
SergeySmirnov75 <122604382+SergeySmirnov75@users.noreply.github.com>
2023-05-19 00:34:40 +0500

    Update Transport.java

--- src/main/java/org/example/Transport.java
+++ src/main/java/org/example/Transport.java
@@ -1,12 +1,8 @@
-public class Transport
-{
-    private String modelName;
-    private int wheelsCount;
-
     public Transport(String modelName, int wheelsCount)
     {
         this.modelName = modelName;
         this.wheelsCount = wheelsCount;
+
     }
 
     public String getModelName()
@@ -27,5 +23,14 @@ public class Transport
     public void setWheelsCount(int wheelsCount)
     {
         this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre()
+    {
+        System.out.println("Меняем покрышку");
+    }
+    public void check()
+    {
+
     }
 }
